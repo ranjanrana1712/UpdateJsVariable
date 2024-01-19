10c00a50222fdf98f2a3b93e0cc22fc30fe006e8
Priya Ranjan Kumar <116024984+ranjanrana1712@users.noreply.github.com>
2024-01-19 12:27:41 +0530

    feature:made responsive

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const inputs = document.querySelectorAll('.controls input');
 
-function handleUpdate() {
-    // this refers to the element that triggered the event
+function handleUpdate(){
     const suffix = this.dataset.sizing || '';
-    console.log(this.name);
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    
 
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
+
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
